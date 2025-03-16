7d61c3b095ac0ac723afce35cb1934c1cfd942d7
ktaschek <ktaschek@gmail.com>
2025-03-16 14:35:06 -0700

    feat: finalize contributor item selection and deletion

--- app/src/main/java/com/splitr/app/ui/SplitrApp.kt
+++ app/src/main/java/com/splitr/app/ui/SplitrApp.kt
@@ -12,6 +12,7 @@ import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
 import androidx.camera.view.PreviewView
+import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -22,9 +23,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
 import androidx.compose.material.icons.filled.Delete
@@ -33,6 +36,7 @@ import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -48,6 +52,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.TextStyle
@@ -64,6 +69,7 @@ import androidx.navigation.compose.rememberNavController
 import androidx.navigation.toRoute
 import com.splitr.app.data.AppDatabase
 import com.splitr.app.data.Item
+import com.splitr.app.data.ItemWithUsers
 import com.splitr.app.data.Receipt
 import com.splitr.app.data.ReceiptWithItems
 import com.splitr.app.data.ReceiptWithItemsAndUsers
@@ -664,6 +670,7 @@ fun DistributeReceiptScreen(
 ) {
     // State to hold the collaborators selected in this screen
     val receiptContributors = viewModel.receiptContributors.collectAsState()
+    var selectedItemId by remember { mutableStateOf<Int?>(null) }
 
     LaunchedEffect(receiptWithItemsAndUsers.receipt.id) {
         viewModel.loadReceipt(receiptWithItemsAndUsers.receipt.id)
@@ -694,7 +701,13 @@ fun DistributeReceiptScreen(
         ) {
             LazyColumn(modifier = Modifier.padding(16.dp)) {
                 items(receiptWithItemsAndUsers.itemsWithUsers) { itemWithUsers ->
-                    Text("${itemWithUsers.item.name} - ${itemWithUsers.users}")
+                    ReceiptItemWithContributors(
+                        itemWithUsers = itemWithUsers,
+                        onItemClick = { selectedItemId = itemWithUsers.item.id },
+                        onContributorClick = { user ->
+                            viewModel.removeCollaboratorsOnItem(itemWithUsers.item.id, user.id)
+                        }
+                    )
                 }
             }
         }
@@ -710,7 +723,16 @@ fun DistributeReceiptScreen(
         if (receiptContributors.value.isNotEmpty()) {
             Column(modifier = Modifier.padding(8.dp)) {
                 receiptContributors.value.forEach { collaborator ->
-                    Text(collaborator.name)
+                    CollaboratorItem(
+                        collaborator,
+                        onClick = ({
+                            viewModel.assignCollaboratorsOnItem(
+                                selectedItemId,
+                                collaborator.id
+                            )
+                        }),
+                        onDelete = ({ viewModel.deleteCollaborator(collaborator) })
+                    )
                 }
             }
         } else {
@@ -800,4 +822,74 @@ fun CollaboratorItem(
             }
         }
     }
+}
+
+@Composable
+fun ItemCollaboratorIcon(user: User, onClick: () -> Unit) {
+    Card(
+        modifier = Modifier
+            .size(40.dp)  // Adjust the size as needed
+            .clip(CircleShape)
+            .border(width = 2.dp, color = Color.Gray, shape = CircleShape)
+            .clickable { onClick() },
+        shape = CircleShape,
+        elevation = CardDefaults.cardElevation(defaultElevation = 4.dp)
+    ) {
+        Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            Text(
+                text = user.name.firstOrNull()?.toString() ?: "",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Medium
+            )
+        }
+    }
+}
+
+@Composable
+fun ReceiptItemWithContributors(
+    itemWithUsers: ItemWithUsers,
+    onItemClick: () -> Unit,
+    onContributorClick: (User) -> Unit
+) {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable { onItemClick() },
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(8.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            // Item name on the left
+            Text(
+                text = itemWithUsers.item.name,
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+            // Spacer takes up all available space, pushing the icons to the right
+            Spacer(modifier = Modifier.weight(1f))
+            // Row for contributor icons on the right, spaced out as desired
+            Row(horizontalArrangement = Arrangement.spacedBy(8.dp)) {
+                itemWithUsers.users.forEach { user ->
+                    ItemCollaboratorIcon(
+                        user = user,
+                        onClick = { onContributorClick(user) }
+                    )
+                }
+            }
+        }
+    }
+}
+
+//TODO: Implement ReceiptBreakdownScreen
+@Composable
+fun ReceiptBreakdownScreen(
+
+) {
+
 }
--- app/src/main/java/com/splitr/app/ui/theme/DistributeReceiptViewModel.kt
+++ app/src/main/java/com/splitr/app/ui/theme/DistributeReceiptViewModel.kt
@@ -5,12 +5,15 @@ import androidx.lifecycle.viewModelScope
 import com.splitr.app.data.ReceiptDao
 import com.splitr.app.data.ReceiptWithItemsAndUsers
 import com.splitr.app.data.User
+import com.splitr.app.data.UserItemCrossRef
+import com.splitr.app.data.UserReceiptCrossRef
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.launch
 
+
 class DistributeReceiptViewModel(
     private val receiptDao: ReceiptDao,
-    private val receiptId: Int?
+    private val receiptId: Int
 ) : ViewModel() {
 
     private val _receiptWithItemsAndUsers = MutableStateFlow<ReceiptWithItemsAndUsers?>(null)
@@ -21,9 +24,7 @@ class DistributeReceiptViewModel(
     val receiptContributors: MutableStateFlow<List<User>> = _receiptContributors
 
     init {
-        receiptId?.let {
-            loadReceipt(it)
-        }
+        loadReceipt(receiptId)
     }
 
     fun loadReceipt(receiptId: Int) {
@@ -35,8 +36,41 @@ class DistributeReceiptViewModel(
         }
     }
 
-    fun assignCollaborators(receiptId: Int) {
-        //TODO:
+    fun deleteCollaborator(user: User) {
+        viewModelScope.launch {
+            _receiptWithItemsAndUsers.value?.itemsWithUsers?.forEach { itemWithUsers ->
+                itemWithUsers.users.forEach { itemUser ->
+                    if (itemUser.id == user.id) {
+                        receiptDao.deleteUserItemCrossRef(
+                            UserItemCrossRef(
+                                user.id,
+                                itemWithUsers.item.id
+                            )
+                        )
+                    }
+                }
+            }
+
+            receiptDao.deleteUserReceiptCrossRef(UserReceiptCrossRef(user.id, receiptId))
+            _receiptContributors.value = receiptDao.getUsersForReceiptById(receiptId)
+            _receiptWithItemsAndUsers.value = receiptDao.getReceiptWithUsersById(receiptId)
+        }
+    }
+
+    fun assignCollaboratorsOnItem(itemId: Int?, userId: Int) {
+        if (itemId != null) {
+            viewModelScope.launch {
+                receiptDao.insertUserItemCrossRef(UserItemCrossRef(userId, itemId))
+                _receiptWithItemsAndUsers.value = receiptDao.getReceiptWithUsersById(receiptId)
+            }
+        }
+    }
+
+    fun removeCollaboratorsOnItem(itemId: Int, userId: Int) {
+        viewModelScope.launch {
+            receiptDao.deleteUserItemCrossRef(UserItemCrossRef(userId, itemId))
+            _receiptWithItemsAndUsers.value = receiptDao.getReceiptWithUsersById(receiptId)
+        }
     }
 
 }
